2243e74ea322617a40a7440e5c2db3d7156e991d
JiminLee411 <wlals41189@gmail.com>
2019-09-10 11:37:56 +0900

    09 | Set URL namespace

--- articles/views.py
+++ articles/views.py
@@ -23,7 +23,7 @@ def create(request):
     # context = {
     #     'article': article
     # }
-    return redirect(f'/articles/{article.pk}/')
+    return redirect('articles:detail', article.pk)
 
 def detail(request, article_pk):
     article = Article.objects.get(pk=article_pk)
@@ -36,7 +36,7 @@ def delete(request, article_pk):
     article = Article.objects.get(pk=article_pk)
     article.delete()
 
-    return redirect('/articles/')
+    return redirect('articles:index')
 
 def edit(request, article_pk):
     article = Article.objects.get(pk=article_pk)
@@ -52,4 +52,4 @@ def update(request, article_pk):
     article.title = title
     article.content = content
     article.save()
-    return redirect(f'/articles/{article.pk}/')
+    return redirect('articles:detail', article.pk)
