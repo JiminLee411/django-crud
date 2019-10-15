e3e37fd5c4c29309387c1e0e4fcbbc1b65e6ebff
JiminLee411 <wlals41189@gmail.com>
2019-10-15 14:09:19 +0900

    26 | Accounts login

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.forms import AuthenticationForm
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm # UserCreationForm : 회원가입 폼,  AuthenticationForm : 인증과 관련된 form
+from django.contrib.auth import login as auth_login
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
-        signup_form = UserCreationForm(request.POST)
-        if signup_form.is_valid():
-            signup_form.save()
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
             return redirect('articles:index')
     else:
-        signup_form = UserCreationForm()
+        form = UserCreationForm()
     context = {
-        'signup_form' : signup_form
+        'form' : form
     }
     return render(request, 'accounts/signup.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            # 로그인
+            user = form.get_user()
+            auth_login(request, user) # request에 login관련 정보가 다 들어있다.
+            return redirect('articles:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'accounts/login.html', context)
